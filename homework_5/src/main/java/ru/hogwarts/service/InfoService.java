cc6c6e072c01c2e202d83b42b002e8a157d9ddbd
hardr1se <stepan-morozov-1980@mail.ru>
2023-07-19 19:21:39 +0400

    ...

--- homework_5/src/main/java/ru/hogwarts/service/InfoService.java
+++ homework_5/src/main/java/ru/hogwarts/service/InfoService.java
@@ -8,6 +8,7 @@ import java.util.stream.Stream;
 public class InfoService {
     public int otherTask() {
         return Stream.iterate(1, a -> a + 1)
+                .parallel()
                 .limit(1_000_000)
                 .mapToInt(Integer::intValue)
                 .sum();
