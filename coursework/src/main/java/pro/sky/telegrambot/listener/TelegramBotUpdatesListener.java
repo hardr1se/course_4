8b2ff7888d5fb517f95518c5acf6985198949b69
HardR1se <stepan_morozov_1980@mail.ru>
2023-08-13 14:03:39 +0400

    correction

--- coursework/src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ coursework/src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -3,44 +3,40 @@ package pro.sky.telegrambot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import pro.sky.telegrambot.enities.NotificationTask;
+import pro.sky.telegrambot.dto.NotificationTaskDtoIn;
 import pro.sky.telegrambot.mapper.NotificationTaskMapper;
 import pro.sky.telegrambot.repositories.NotificationTaskRepository;
-import pro.sky.telegrambot.utils.MessageUtils;
 
 import javax.annotation.PostConstruct;
 import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
-
     private final Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
-    private long chatId = 0;
+    private final static Pattern PATTERN = Pattern.compile("(\\d{1,2}\\.\\d{1,2}\\.\\d{4} \\d{1,2}:\\d{2}) ([А-яA-z\\d,\\s]+)");
+    private final static DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
 
     private final TelegramBot telegramBot;
     private final NotificationTaskRepository taskRepository;
     private final NotificationTaskMapper taskMapper;
     private final TelegramBot bot;
-    private final MessageUtils messageUtils;
-
 
     public TelegramBotUpdatesListener(TelegramBot telegramBot,
                                       NotificationTaskRepository taskRepository,
                                       NotificationTaskMapper taskMapper,
-                                      TelegramBot bot, MessageUtils messageUtils) {
+                                      TelegramBot bot) {
         this.telegramBot = telegramBot;
         this.taskRepository = taskRepository;
         this.taskMapper = taskMapper;
         this.bot = bot;
-        this.messageUtils = messageUtils;
     }
 
     @PostConstruct
@@ -50,66 +46,39 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
     @Override
     public int process(List<Update> updates) {
-        var pattern = Pattern.compile("([0-9.:\\s]{16})(\\s)([\\W+]+)");
-
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
+            var chatId = update.message().chat().id();
+            if (update.message() != null) {
+                var text = update.message().text();
+                var matcher = PATTERN.matcher(text);
+                LocalDateTime date;
+                if (text.equals("/start")) {
+                    bot.execute(new SendMessage(chatId, "Hi there, It's my course work. To get the pattern of correct task formation print command /add."));
+                } else if (text.equals("/add")) {
+                    bot.execute(new SendMessage(chatId, "Write ur task according to a certain example: 01.01.2022 20:00 Помыть посуду"));
+                } else if (matcher.matches() && (date = parseDate(matcher.group(1))) != null) {
+                    NotificationTaskDtoIn dtoIn = new NotificationTaskDtoIn(Math.toIntExact(chatId), text, date);
+                    bot.execute(new SendMessage(
+                            chatId, "Task '" + taskMapper.toDto(taskRepository.save(taskMapper.toEntity(dtoIn))) + "' is successfully added"));
 
-            chatId = update.message().chat().id();
-            var message = update.message().text();
-            var matcher = pattern.matcher(message);
-
-            distributiveMessagesByCommands(chatId, matcher, message);
+                } else {
+                    logger.error("Unsupported format of message");
+                    bot.execute(new SendMessage(chatId, "Unsupported type of file"));
+                }
+            } else {
+                logger.error("Unsupported command");
+                bot.execute(new SendMessage(chatId, "Unsupported command"));
+            }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
 
-    private void distributiveMessagesByCommands(Long chatId, Matcher matcher, String message) {
-        if (message.equals("/start")) {
-            processStartCommand();
-        } else if (message.equals("/add")) {
-            processAddCommand();
-        } else if (matcher.matches()) {
-            processNotificationTask(message);
-        } else {
-            unsupportedTypeOfText();
-        }
-    }
-
-    private void processStartCommand() {
-        bot.execute(messageUtils.sendMessage(chatId, "Hi there, It's my course work. To get the pattern of correct task formation print command /add."));
-    }
-
-    private void processAddCommand() {
-        var sendAddMessage = messageUtils.sendMessage(chatId, "Write ur task according to a certain example: 01.01.2022 20:00 Помыть посуду");
-        bot.execute(sendAddMessage);
-    }
-
-    private void unsupportedTypeOfText() {
-        logger.error("Unsupported command");
-        bot.execute(messageUtils.sendMessage(chatId, "Unsupported command"));
-    }
-
-    private void processNotificationTask(String message) {
-        var taskDtoIn = messageUtils.taskProcessing(chatId, message);
-        if (taskDtoIn == null) {
-            logger.error("Unsupported argument");
-            return;
-        }
-        bot.execute(messageUtils.sendMessage(chatId, String.format(
-                "Task: '%s' - is successfully added",
-                taskMapper.toDto(taskRepository.save(taskMapper.toEntity(taskDtoIn))))));
-    }
-
-    @Scheduled(fixedDelay = 10_000)
-    private synchronized void reminder() {
-        if (chatId != 0) {
-            NotificationTask notificationTask = taskRepository.getByDateOfTask(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES));
-            if (notificationTask != null) {
-                taskRepository.delete(notificationTask);
-                bot.execute(messageUtils.sendMessage(chatId, taskMapper.toDto(notificationTask).toString()));
-                logger.info("Task '" + notificationTask + "' is pushed and deleted from DB");
-            }
+    private LocalDateTime parseDate(String date) {
+        try {
+            return LocalDateTime.parse(date, DATE_TIME_FORMATTER);
+        } catch (DateTimeParseException e) {
+            return null;
         }
     }
 }
